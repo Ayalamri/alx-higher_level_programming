f1bf483b76da0a9b99557059cfa68d7a64243ceb
Ayalamri <ayalamrioui19@gmail.com>
2023-11-29 15:34:28 -0800

    yyy

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    print(f"{chr(i)}{chr(i - 32)}", end='')
+for i in range(ord('z'), ord('A') - 1, -2):
+    print(f"{chr(i)}{chr(i - 33)}", end='')
