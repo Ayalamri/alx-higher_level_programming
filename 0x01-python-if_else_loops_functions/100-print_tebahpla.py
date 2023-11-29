78bb60c97c4ab360730555c1a73e1f0df8ae9220
Ayalamri <ayalamrioui19@gmail.com>
2023-11-29 15:49:34 -0800

    fgg

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('A') - 1, -2):
     print("{}".format(chr(i)), end='')
-    print("{}".format(chr(i - 33)), end='')
+    print("{}".format(chr(i - 25)), end='')
