bfa4e38a134c7acf85c25987e518fa8534f2fc3b
Ayalamri <ayalamrioui19@gmail.com>
2023-12-19 17:13:56 -0800

    eeee

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-Square = __import__('2-square').Square
+"""Square module."""
 
-my_square_1 = Square(3)
-print(type(my_square_1))
-print(my_square_1.__dict__)
 
-my_square_2 = Square()
-print(type(my_square_2))
-print(my_square_2.__dict__)
+class Square:
+    """Defines a square."""
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
+    def __init__(self, size):
+        """Constructor.
 
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-    print(e)
+        Args:
+            size: length of a side of the square.
+        
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than 0
+        """
+        if not isinstance(size, int)
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
