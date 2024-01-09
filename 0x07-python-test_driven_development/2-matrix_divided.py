6ccd8acd0ccb1ec9024b56b7ddba4b6584a0b243
Ayalamri <ayalamrioui19@gmail.com>
2024-01-09 14:05:15 -0800

    rtggy

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,16 @@
-gg
+#!/usr/bin/python3
+'''Module for matrix division.'''
+
+
+def matrix_divided(matrix, div):
+    '''Divides all elements of a matrix by a number.'''
+    if not matrix or not all(matrix) or not all(map(lambda row: len(row) == len(matrix[0]), matrix)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(elem / div, 2) for elem in row] for row in matrix]
