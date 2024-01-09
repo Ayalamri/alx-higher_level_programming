d9eca8f93f8488ff2a1e61b8de09f23f3e9dcca9
Ayalamri <ayalamrioui19@gmail.com>
2024-01-08 16:03:25 -0800

    yyhj

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""
+the class MyInt
+"""
+
+
 class MyInt(int):
+    """version of an inte"""
+    def __new__(cls, *args, **kwargs):
+        """new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
     def __eq__(self, other):
-        return super().__ne__(other)
+        """was != is =="""
+        return int(self) != other
 
     def __ne__(self, other):
-        return super().__eq__(other)
+        """was == is !="""
+        return int(self) == other
