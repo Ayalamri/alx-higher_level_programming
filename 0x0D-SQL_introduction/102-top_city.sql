c8796e4e9784e611e8bb3715a0a1a1909892f826
Ayalamri <ayalamrioui19@gmail.com>
2024-02-14 04:36:09 -0800

    ygjy

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Query to select top 3 cities' temperatures during July and August
+SELECT city, AVG(temperature) AS avg_temp
+FROM temperatures
+WHERE MONTH(date) IN (7, 8)
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
