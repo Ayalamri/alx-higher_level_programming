0b878638076c228c4e3ff3365869f88cf4218826
Ayalamri <ayalamrioui19@gmail.com>
2024-02-14 15:42:19 -0800

    ervgf

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- List all shows without the genre Comedy
+SELECT title
+FROM tv_shows
+WHERE id NOT IN (
+    SELECT show_id
+    FROM shows_genres
+    WHERE genre_id = (
+        SELECT id
+        FROM tv_genres
+        WHERE name = 'Comedy'
+    )
+)
+ORDER BY title ASC;
